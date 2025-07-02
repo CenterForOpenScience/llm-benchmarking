286d7b3f75d7e1c8a7bafb8b33e17743bab30130
Rochana <rochana.odu@gmail.com>
2025-07-02 18:43:51 -0400

    info_extractor 1 and 2 stages merged - completing extraction in one go

--- main.py
+++ main.py
@@ -6,18 +6,18 @@ Created on Mon Jun  9 15:36:52 2025
 @author: Rochana Obadage
 """
 
-from info_extractor.extractor import run_extraction
 import argparse
+from info_extractor.extractor import run_extraction
+
 
 def main():
     parser = argparse.ArgumentParser(description="LLM-based Replication Info Extractor")
     parser.add_argument('--study_path', required=True, help="Path to case study folder")
-    parser.add_argument('--stage', choices=['1', '2'], required=True, help="Stage: 1 or 2")
     parser.add_argument('--difficulty', choices=['easy', 'medium', 'hard'], required=True, help="Difficulty level")
     parser.add_argument("--show-prompt", action="store_true", help="Print the generated prompt and exit")
     args = parser.parse_args()
    
-    run_extraction(study_path=args.study_path, stage=args.stage, difficulty=args.difficulty, show_prompt=args.show_prompt)
+    run_extraction(study_path=args.study_path, difficulty=args.difficulty, show_prompt=args.show_prompt)
 
 
 if __name__ == "__main__":
